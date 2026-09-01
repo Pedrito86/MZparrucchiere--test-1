8a8dc298804ceebe6df7338c7eb7a13c825b832c
Pedrito86 <santuccipietro23@gmail.com>
2026-09-02 00:32:56 +0200

    aggior sedi

---
+++ _mz_verify.py
@@ -0,0 +1,4 @@
+import urllib.request, os
+BASE='http://localhost:8090'
+paths=['/index.html','/salone-villanova-castenaso/','/salone-villafontana-medicina/','/salone-imola/','/barber-shop/']
+print('                                                           TITLE' 
